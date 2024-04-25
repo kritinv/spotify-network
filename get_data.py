b93a4d53843b17ea06ed74df32bff87003f317bb
Kritin Vongthongsri <73642562+kritinv@users.noreply.github.com>
2024-04-25 15:23:20 -0400

    data retrieval optimizations: caching and sleeping

--- get_data.py
+++ get_data.py
@@ -4,6 +4,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 from tqdm import tqdm
+import time
 
 ###############################################
 # Before you run get_data.py
@@ -18,12 +19,6 @@ from tqdm import tqdm
 #   SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
 #   SPOTIPY_REDIRECT_URI='your-app-redirect-url' (I used http://localhost)
 #   ...........
-
-
-
-#   SPOTIPY_CLIENT_ID='cd577af44aa748d98b50df5fc60efcad'
-#   SPOTIPY_CLIENT_SECRET='41ef48a4f63f4b27915ac3d387cd7b86'
-#   SPOTIPY_REDIRECT_URI='http://localhost:3000'
 
 load_dotenv()
 
@@ -53,39 +48,34 @@ class Artist:
 ###############################################
 
 # Authentication
-client_credentials_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, requests_timeout=30)
+auth_manager  = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=auth_manager, requests_timeout=5, retries=10)
 
 # These starting artists were top artists in each year's Billboard Year End Hot 100 Playlist
 # If a song had multiple artists, only the first was selected
 # Wnet down the list until 5 distinct artists were obtained
-# starting_artists = {
-# 	"2013": ["5BcAKTbp20cv7tC5VqPFoC", "0ZrpamOxcZybMHGg1AYtHP", "53XhwfbYqKCa1cC15pYq2q", "25fqWEebq6PoiGQIHIrdtv", "31TPClRtHm23RisEBtV3X7"],
-# 	"2014": ["2RdwBSPQiwcmiDo9kixcl8", "6jJ0s89eD6GaHleKKya26X", "5y2Xq6xcjJb2jVM54GHK3t", "5yG7ZAZafVaAlMTeBybKAL", "5Pwc4xIPtQLFEnJriah9YJ"],
-# 	"2015": ["3hv9jJF3adDNsBSIQDqcjp", "6eUKZXaKkcviH0Ku9w2n3V", "137W8MRPWKqSmrBGDBFSop", "6PXS4YHDkKvl1wkIl4V8DL", "04gDigrS5kc9YWfZHwBETP"],
-# 	"2016": ["1uNFoZAHBGtllmzznpCI3s", "3TVXtAsR1Inumwj472S9r4", "5pKCCKE2ajJHZ9KAiaK11H", "3YQKmKGau1PzlVlkL1iodx", "7pFeBzX627ff0VnN6bxPR4"],
-#     "2017": ["6eUKZXaKkcviH0Ku9w2n3V", "4V8Sr092TqfHkfAA5fXXqG", "0du5cEVh5yTK9QJze8zA0C", "2YZyLoL8N0Wb9xBt1NhZWg", "69GGBxA162lTqCwzJG5jLp"],
-#     "2018": ["3TVXtAsR1Inumwj472S9r4", "6eUKZXaKkcviH0Ku9w2n3V", "64M6ah0SkkRsnPGtGiRAbb", "4nDoRrQiYLoBzwC5BhVJzF", "246dkjvS1zLTtiykXe5h60"],
-#     "2019": ["7jVv8c5Fj3E9VhNjxT4snq", "246dkjvS1zLTtiykXe5h60", "26VFTg2z8YR0cCuwLzESi2", "6qqNVTkY8uBg9cP3Jd7DAH", "64KEffDW9EtZ1y2vBYgq8T"],
-#     "2020": ["1Xyo4u8uXC1ZmMpatF05PJ", "246dkjvS1zLTtiykXe5h60", "757aE44tKEUQEqRuT6GnEB", "6M2wZ9GZgrQXHCFfjv46we", "4r63FhuTkUYltbVAg5TQnk"],
-#     "2021": ["6M2wZ9GZgrQXHCFfjv46we", "1Xyo4u8uXC1ZmMpatF05PJ", "6fWVd57NKTalqvmjRd2t8Z", "1McMsnEElThX1knmY4oliG", "5cj0lLjcoR7YOSnhnX0Po5"],
-# 	"2022": ["4yvcSjfu4PC0CYQyLy4wSq", "6KImCVD70vtIoJWnq6nGn3", "2tIP7SsRs7vjIcLrU85W8J", "4dpARuHxo51G3z768sgnrY", "6eUKZXaKkcviH0Ku9w2n3V"],
-# 	"2023": ["4oUHIQIBe0LHzYfvXNW4QM", "5YGY8feqx7naU7z4HrwZM6", "7tYKF4w9nC0nq9CsPZTHyP", "06HL4z0CvFAxyc27GXpf02", "0iEtIxbK0KxaSlF7G42ZOp"],
-# }
-
 starting_artists = {
-	# "2016": ["1uNFoZAHBGtllmzznpCI3s", "3TVXtAsR1Inumwj472S9r4", "5pKCCKE2ajJHZ9KAiaK11H", "3YQKmKGau1PzlVlkL1iodx", "7pFeBzX627ff0VnN6bxPR4"],
-    # "2017": ["6eUKZXaKkcviH0Ku9w2n3V", "4V8Sr092TqfHkfAA5fXXqG", "0du5cEVh5yTK9QJze8zA0C", "2YZyLoL8N0Wb9xBt1NhZWg", "69GGBxA162lTqCwzJG5jLp"],
-    "2018": ["3TVXtAsR1Inumwj472S9r4", "6eUKZXaKkcviH0Ku9w2n3V", "64M6ah0SkkRsnPGtGiRAbb", "4nDoRrQiYLoBzwC5BhVJzF", "246dkjvS1zLTtiykXe5h60"],
-}
+	"2013": ["5BcAKTbp20cv7tC5VqPFoC", "0ZrpamOxcZybMHGg1AYtHP", "53XhwfbYqKCa1cC15pYq2q", "25fqWEebq6PoiGQIHIrdtv", "31TPClRtHm23RisEBtV3X7"],
+	"2014": ["2RdwBSPQiwcmiDo9kixcl8", "6jJ0s89eD6GaHleKKya26X", "5y2Xq6xcjJb2jVM54GHK3t", "5yG7ZAZafVaAlMTeBybKAL", "5Pwc4xIPtQLFEnJriah9YJ"],
+	"2015": ["3hv9jJF3adDNsBSIQDqcjp", "6eUKZXaKkcviH0Ku9w2n3V", "137W8MRPWKqSmrBGDBFSop", "6PXS4YHDkKvl1wkIl4V8DL", "04gDigrS5kc9YWfZHwBETP"],
+	"2016": ["1uNFoZAHBGtllmzznpCI3s", "3TVXtAsR1Inumwj472S9r4", "5pKCCKE2ajJHZ9KAiaK11H", "3YQKmKGau1PzlVlkL1iodx", "7pFeBzX627ff0VnN6bxPR4"],
+	"2017": ["6eUKZXaKkcviH0Ku9w2n3V", "4V8Sr092TqfHkfAA5fXXqG", "0du5cEVh5yTK9QJze8zA0C", "2YZyLoL8N0Wb9xBt1NhZWg", "69GGBxA162lTqCwzJG5jLp"],
+	"2018": ["3TVXtAsR1Inumwj472S9r4", "6eUKZXaKkcviH0Ku9w2n3V", "64M6ah0SkkRsnPGtGiRAbb", "4nDoRrQiYLoBzwC5BhVJzF", "246dkjvS1zLTtiykXe5h60"],
+	"2019": ["7jVv8c5Fj3E9VhNjxT4snq", "246dkjvS1zLTtiykXe5h60", "26VFTg2z8YR0cCuwLzESi2", "6qqNVTkY8uBg9cP3Jd7DAH", "64KEffDW9EtZ1y2vBYgq8T"],
+	"2020": ["1Xyo4u8uXC1ZmMpatF05PJ", "246dkjvS1zLTtiykXe5h60", "757aE44tKEUQEqRuT6GnEB", "6M2wZ9GZgrQXHCFfjv46we", "4r63FhuTkUYltbVAg5TQnk"],
+	"2021": ["6M2wZ9GZgrQXHCFfjv46we", "1Xyo4u8uXC1ZmMpatF05PJ", "6fWVd57NKTalqvmjRd2t8Z", "1McMsnEElThX1knmY4oliG", "5cj0lLjcoR7YOSnhnX0Po5"],
+	"2022": ["4yvcSjfu4PC0CYQyLy4wSq", "6KImCVD70vtIoJWnq6nGn3", "2tIP7SsRs7vjIcLrU85W8J", "4dpARuHxo51G3z768sgnrY", "6eUKZXaKkcviH0Ku9w2n3V"],
+	"2023": ["4oUHIQIBe0LHzYfvXNW4QM", "5YGY8feqx7naU7z4HrwZM6", "7tYKF4w9nC0nq9CsPZTHyP", "06HL4z0CvFAxyc27GXpf02", "0iEtIxbK0KxaSlF7G42ZOp"],
+	}
 
 # Parameters
-# max_artists = 5000
-max_artists = 500
+max_artists = 1000
 follower_threshold = 1000
 queues = {year: [] for year in starting_artists.keys()}
 artist_sets = {year: set() for year in starting_artists.keys()}
 artist_maps = {year: defaultdict(list) for year in starting_artists.keys()}
+valid_artists = {year: {} for year in starting_artists.keys()}
+
 
 for year, artists in starting_artists.items():
 	print("Initializing " + str(year))
@@ -94,13 +84,23 @@ for year, artists in starting_artists.items():
 		cur_artist = Artist(cur_id, cur["name"], cur["genres"], cur["followers"]["total"])
 		artist_sets[year].add(cur_artist)
 		queues[year].append(cur_artist)
+		valid_artists[year][cur_artist.artist_id] = True
 
 ###############################################
 # BFS on Recommended Artists
 ###############################################
 
-def artist_active_in_year(spotify_client, artist_id, year):
-	albums = spotify_client.artist_albums(artist_id, album_type='album')
+def artist_is_valid(artist: Artist):
+	valid = True
+	if artist.followers < follower_threshold:
+		valid = False
+	if not artist_active_in_year(sp, artist.artist_id, year):
+		valid = False
+	return valid
+
+def artist_active_in_year(sp, artist_id, year):
+	albums = sp.artist_albums(artist_id, album_type='album')
+	#time.sleep(1)
 	for album in albums['items']:
 		release_date = album['release_date']  # Release date format can be "YYYY-MM-DD", "YYYY-MM", or "YYYY"
 		if release_date.startswith(str(year)):
@@ -112,22 +112,33 @@ for year, queue in queues.items():
 	while queue and len(artist_sets[year]) < max_artists:
 
 		cur_artist = queue.pop(0)
-		artists = sp.artist_related_artists(cur_artist.artist_id)
-		
-		for artist in artists["artists"]:
+		related_artists = sp.artist_related_artists(cur_artist.artist_id)
+		#time.sleep(1)
+
+		for r in related_artists["artists"]:
+			artist = Artist(r["id"], r["name"], r["genres"], r["followers"]["total"])
 			
-			if artist["followers"]["total"] < follower_threshold:
+			# check if artist has already been processed and is valid
+			is_valid = True
+			if artist.artist_id in valid_artists[year].keys():
+				print('cached')
+				is_valid = artist_maps[year][artist.artist_id]
+			else:
+				is_valid = artist_is_valid(artist)
+				valid_artists[year][artist.artist_id] = is_valid
+			if not is_valid:
 				continue
-			if not artist_active_in_year(sp, artist["id"], year):
-				continue
 			
-			related = Artist(artist["id"], artist["name"], artist["genres"], artist["followers"]["total"])
-			artist_maps[year][cur_artist.artist_id].append(related.artist_id)
+			# add valid artist to cur_artist id in artist_maps
+			artist_maps[year][cur_artist.artist_id].append(artist.artist_id)
 			
-			if related not in artist_sets[year]:
-				queue.append(related)  # Append tuple of (Artist, year)
+			# add to unique artists_set
+			if artist not in artist_sets[year]:
+				queue.append(artist)  # Append tuple of (Artist, year)
 				pbar.update(1)
-			artist_sets[year].add(related)
+				artist_sets[year].add(artist)
+		
+
 			
 	artist_map = artist_maps[year] 
 	json.dump(artist_map, open(f"related_{year}.json", "w+"), indent=4)
